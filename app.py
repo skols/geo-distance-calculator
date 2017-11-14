ace3a040110533446d4458f10329413ec4698957
Michael Skolnik <mikeskolnik75@gmail.com>
2017-11-13 19:44:31 -0500

    Second commit - Changed environment from virtualenv to conda

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from geopy.geocoders import Nominatim
-from geopy.distance import vincenty, great_circle
+# from geopy.distance import vincenty
 import pandas as pd
 # from glob import glob
-# import os
+import os
 
+os.chdir("C:/Development/Personal/geocoding")
 
 geolocator = Nominatim()
 # os.chdir = ("C:/Development/Personal/geocoding/input/")
@@ -27,7 +28,13 @@ df["ToZip_Latitude"] = df["ToZip"].apply(geolocator.geocode).apply(
 df["ToZip_Longitude"] = df["ToZip"].apply(geolocator.geocode).apply(
     lambda x: x.longitude if x is not None else None)
 
-df["Vincenty_Distance"] = vincenty(tuple(df["FromZip_Latitude"],
-                                         df["FromZip_Longitude"]),
-                                   tuple(df["ToZip_Latitude"],
-                                         df["ToZip_Longitude"])).miles
+df_from_coords = df.iloc[:, [2, 3]]
+df_to_coords = df.iloc[:, [4, 5]]
+
+from_coords = tuple([tuple(df_from_coords[col]) for col in df_from_coords])
+print(from_coords)
+# print(df_from_coords)
+# print("*" * 80)
+# print(df_to_coords)
+
+# df["Vincenty_Distance"] = vincenty().miles
